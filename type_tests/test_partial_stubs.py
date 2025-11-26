6aff41fb10738a02ed4dd117cfdce3dca084e6ce
Michael Grinshpon <mgrinshpon@gmail.com>
2025-11-26 06:07:41 -0500

    Move src/toolz to src/toolz-stubs for a more PEP516 compliant package; remove customized pyrightconfig proving that our code works as expected under normal conditions; commented out tests for things not yet typed Closes #6

--- type_tests/test_partial_stubs.py
+++ type_tests/test_partial_stubs.py
@@ -1,25 +1,27 @@
 """Verify that partial stubs are found for both toolz and tlz."""
 
 # Check which modules are imported
-import tlz
+# TODO: Uncomment when tlz stubs are added
+# import tlz
 
 import toolz
 
 print("toolz location:", toolz.__file__)
-print("tlz location:", tlz.__file__)
+# print("tlz location:", tlz.__file__)  # TODO: Uncomment when tlz stubs are added
 
 # Check if stub files exist alongside them
 import os
 
 toolz_dir = os.path.dirname(toolz.__file__)
-tlz_dir = os.path.dirname(tlz.__file__)
+# tlz_dir = os.path.dirname(tlz.__file__)  # TODO: Uncomment when tlz stubs are added
 
 print("\ntoolz stub files:")
 for file in os.listdir(toolz_dir):
     if file.endswith(".pyi"):
         print(f"  {file}")
 
-print("\ntlz stub files:")
-for file in os.listdir(tlz_dir):
-    if file.endswith(".pyi"):
-        print(f"  {file}")
+# TODO: Uncomment when tlz stubs are added
+# print("\ntlz stub files:")
+# for file in os.listdir(tlz_dir):
+#     if file.endswith(".pyi"):
+#         print(f"  {file}")
